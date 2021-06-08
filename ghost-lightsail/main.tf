248eca5cd25dd2707c274f3e80e89a70293ce71f
Phillip Hocking <phillhocking@gmail.com>
2021-06-08 14:28:52 -0700

    Route 53 changes and clarifying comments

--- ghost-lightsail/main.tf
+++ ghost-lightsail/main.tf
@@ -19,13 +19,18 @@ resource "aws_lightsail_static_ip_attachment" "static_ip_attach" {
 }
 
 ## Creates a new Route53 Public Hosted Zone. Comment out for existing zone.
-resource "aws_route53_zone" "lightsail_dev_zone" {
-name = var.domain_name 
+#resource "aws_route53_zone" "lightsail_dev_zone" {
+#name = var.domain_name 
+#}
+
+## Points to your existing Route 53 public hosted zone. Comment this block if you are creating a new Public Hosted Zone
+data "aws_route53_zone" "hosted_zone" {
+  name = "${var.domain_name}." 
 }
 
 resource "aws_route53_record" "red_no_www" {
-  zone_id = aws_route53_zone.lightsail_dev_zone.zone_id 
-  name    = aws_route53_zone.lightsail_dev_zone.name    
+  zone_id = data.aws_route53_zone.lightsail_dev_zone.zone_id  ## Delete prepending "data." if you are creating a new hosted zone
+  name    = data.aws_route53_zone.lightsail_dev_zone.name     ## Delete prepending "data." if you are creating a new hosted zone
   type    = "A"
   ttl     = "300"
   records = [aws_lightsail_static_ip.dev_static_ip.ip_address]
