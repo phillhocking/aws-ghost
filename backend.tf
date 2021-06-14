5b8311925e647edd6f2c1b15f9c944c155e8e322
Phillip Hocking <phillhocking@gmail.com>
2021-06-14 09:11:09 -0700

    Changing backend to phillhocking instead of excelsior tfcloud org

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = "excelsior"
+    organization = "phillhocking"
 
     workspaces {
-      name = "phillhocking-com-dev"
+      name = "aws-ghost-dev"
     }
   }
 }
